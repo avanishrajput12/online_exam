adcd31b36825089511b2308e0eaf15ad74c39972
Avanish Rajput <avanishrajput208017@gmail.com>
2025-11-12 01:52:14 +0530

    12-11-2025

--- resources/views/admin/questions.blade.php
+++ resources/views/admin/questions.blade.php
@@ -7,27 +7,16 @@
     <thead class="table-primary">
       <tr>
         <th>#</th>
-        <th>Question</th>
-        <th>Option A</th>
-        <th>Option B</th>
-        <th>Option C</th>
-        <th>Option D</th>
-        <th>Correct</th>
+        <th>Category</th>
         <th>Actions</th>
       </tr>
     </thead>
     <tbody>
       <tr>
         <td>1</td>
-        <td>What is Laravel?</td>
-        <td>Framework</td>
-        <td>CMS</td>
-        <td>Library</td>
-        <td>Plugin</td>
-        <td>A</td>
+        <td>Aptitude</td>
         <td>
-          <button class="btn btn-sm btn-info text-white"><i class="fa-solid fa-pen"></i></button>
-          <button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></button>
+          <button class="btn btn-sm btn-info text-white">Add Questions</button>
         </td>
       </tr>
     </tbody>
